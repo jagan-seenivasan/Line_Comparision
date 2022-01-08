a4c11b93c065a6aa44491ef90e61b6714f1afddf
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-08 22:31:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Line_Comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CalculateXandYLength uc1 = new CalculateXandYLength();
+            CalculateXandYLength.LengthOfXandY();
+            CheckEqualofLines UC2 = new CheckEqualofLines();
+            CheckEqualofLines.CheckLinesOrEqual();
+            GreaterOrLessThanInLine UC3 = new GreaterOrLessThanInLine();
+            GreaterOrLessThanInLine.CheckLineisGreaterOrLess();
+
+        }
+    }
+}
